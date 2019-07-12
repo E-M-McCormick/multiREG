837fbf058760a4821b974c56675cfa7f97de29b2
Ethan McCormick <emccormick20@gmail.com>
2019-07-12 12:44:20 -0400

    New tests

--- multiLASSO/tests/testthat.R
+++ multiLASSO/tests/testthat.R
@@ -1,11 +1,3 @@
 library(testthat)
 library(multiLASSO)
 
-test_check("multiLASSO")
-
-#-------------------------------------------------------# 
-context("Check Output Structure")
-#-------------------------------------------------------# 
-output = multiLASSO(data = data("HRFsim"), header = FALSE, plot = FALSE)
-
-expect_equal(length(output), 50)
--- multiLASSO/tests/testthat/test-001.R
+++ multiLASSO/tests/testthat/test-001.R
@@ -1,2 +1,10 @@
 library(multiLASSO)
 
+test_check("multiLASSO")
+
+#-------------------------------------------------------# 
+context("Check Output Structure")
+#-------------------------------------------------------# 
+output = multiLASSO(data = data("HRFsim"), header = FALSE, plot = FALSE)
+
+expect_equal(length(output), 50)
