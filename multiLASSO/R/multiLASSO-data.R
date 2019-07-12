34f46f192d5c9461e919a0981da7abf8ad08bac5
Ethan McCormick <emccormick20@gmail.com>
2019-07-12 11:37:44 -0400

    Data documentation

---
+++ multiLASSO/R/multiLASSO-data.R
@@ -0,0 +1,23 @@
+#' Small Node example with exogenous variable
+#'
+#' This object contains a list of time series data for twenty-five individuals 
+#' with 500 time points and 5 variables.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name HRFsim
+#' @usage HRFsim
+#' @format A list of data frames with 25 individuals, who each have 500 observations on 5 variables.
+NULL
+
+#' Medium Node example with 2 exogenous variables
+#'
+#' This object contains a list of time series data for forty-eight individuals 
+#' with 910 time points and 11 variables.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name sitNeuro
+#' @usage sitNeuro
+#' @format A list of data frames with 48 individuals, who each have 910 observations on 11 variables.
+NULL
