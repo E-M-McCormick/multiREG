bdc9ec47f484b11b4be3b2a6d6630d426f1625b1
McCormickNeuro <emccormick@unc.edu>
2019-11-16 22:23:39 -0500

    update how ints are subset

--- multiREG/R/ind_search.R
+++ multiREG/R/ind_search.R
@@ -1,10 +1,7 @@
 #' Find individual-level solutions using final patterns. 
 #' @param subdata Data from previous step.
 #' @param yvarnames Endogenous variable names.
-#' @param interact_exogenous Exogenous variable to use as moderators.
-#' @param predict_with_interactions Endogenous variables to be predicted by interactions.
 #' @param interactnames Names of interaction variables.
-#' @param interact_exogvars Exogenous variable to use as moderators.
 #' @param grppen Group path penalties.
 #' @param output Output object
 #' @param verbose Logical. If TRUE, algorithm will print progress to console.
@@ -12,10 +9,7 @@
 #' @keywords internal  
 ind_search = function(subdata,
                       yvarnames,
-                      interact_exogenous,
-                      predict_with_interactions,
                       interactnames,
-                      interact_exogvars,
                       grppen,
                       output,
                       verbose){
@@ -33,24 +27,16 @@ ind_search = function(subdata,
     if(verbose){print(paste0('Building individual-level model for ', sub, '.'), quote = FALSE)}
     tempdata = subdata[[sub]]
     for (varname in yvarnames){
-      subset_predictors = as.matrix(tempdata[, !(colnames(tempdata) %in% varname |
-                                                   colnames(tempdata) %in% paste0(varname,'_by_',interact_exogvars))])
-      if (!is.null(predict_with_interactions) & !varname %in% predict_with_interactions){
-        subset_predictors = subset_predictors[, !colnames(subset_predictors) %in% interactnames]
-      }
+      subset_index = !(colnames(tempdata) %in% varname |
+                         colnames(tempdata) %in% interactnames[grepl(paste0('\\<',varname,'_by'), interactnames)] | 
+                         colnames(tempdata) %in% interactnames[grepl(paste0('by_',varname,'\\>'), interactnames)] |
+                         colnames(tempdata) %in% interactnames[grepl(paste0('by_',varname,'_by'), interactnames)])
       
-      subset_penalties = grppen[!(colnames(tempdata) %in% varname |
-                                           colnames(tempdata) %in% paste0(varname,'_by_',interact_exogvars)), varname]
-       
-      if (!is.null(predict_with_interactions) & !varname %in% predict_with_interactions){
-        subset_penalties = subset_penalties[!names(subset_penalties) %in% interactnames]
-      }
-      
-      final_coefs = model_selection(x = subset_predictors,
+      final_coefs = model_selection(x = as.matrix(tempdata[, subset_index]),
                                     y = tempdata[, colnames(tempdata) %in% varname],
                                     selection_crit = model_crit,
                                     alpha = alpha,
-                                    penalty.factor = subset_penalties)
+                                    penalty.factor = initial_penalties[subset_index, varname])
       
       for (predictor in rownames(final_coefs)[!rownames(final_coefs) %in% '(Intercept)']){
         if (final_coefs[predictor,] != 0){
